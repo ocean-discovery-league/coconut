f9cb67c0d4d222afdc4315ebc8fc916209e77d3c
Jon Ferguson <jon@jef.us>
2021-12-27 20:11:39 +0000

    WIP moving to the pi zero 2 so newer node will work

--- install-node.sh
+++ install-node.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
 
-mkdir node
+node_version=16.13.1
+
+mkdir -p node
 git clone https://github.com/tj/n node/n
 (export PREFIX=`pwd`/node; cd node/n; make; make install)
-N_PREFIX=`pwd`/node node/bin/n 10.23
+
+#pi zero arch no longer officially supported on node versions > 11
+#https://raspberrypi.stackexchange.com/questions/111130/how-to-install-nodejs-v12-on-raspi-zero-armv6-not-available-for-download-anymor
+# download the unofficial build and stick it into n's downloaded versions directory
+# so it'll find it there and not try to look for it at nodejs.org and fail
+if [[ `uname -m` -eq "arm6l" ]]; then
+    echo "Downloading nodejs ${node_version} for armv6 from unofficial builds..."
+    mkdir -p node/n/versions/node
+    (cd node/n/versions/node
+     curl -O https://unofficial-builds.nodejs.org/download/release/v${node_version}/node-v${node_version}-linux-armv6l.tar.xz
+     tar -xf node-v${node_version}-linux-armv6l.tar.xz
+     mv node-v${node_version}-linux-armv6l ${node_version}
+     rm node-v${node_version}-linux-armv6l.tar.xz
+    )
+fi
+
+N_PREFIX=`pwd`/node node/bin/n $node_version
+
+# arm6l version of node crashes because of no wasm support
+if [[ `name -m` -eq "arm6l" ]]; then
+    mv node/bin/node{,.actual}
+    cat > node/bin/node <EOF
+#!/bin/sh
+node_actual="`dirname $0`/node.actual"
+exec "${node_actual}" --no-expose-wasm $*
+EOF
+    chmod +x node/bin/node
+fi
+
 source ./activate
 npm install -g yarn
---
+++ patches/README.md
@@ -0,0 +1,2 @@
+The preview.php patch requires a chmod g+w on the /var/www/html/media directory
+itself for the deleteAll fix to work.
