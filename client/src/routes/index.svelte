014aca220c5767392d539a8a884ee360f5e837ca
Jon Ferguson <jon@jef.us>
2022-03-10 10:38:33 -0500

    hotfix so arrow keys work in text fields

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,4 +1,4 @@
-<script>
+ <script>
   import { onMount, onDestroy } from 'svelte';
   import { browser } from '$app/env';
   import Page from '$lib/Page.svelte';
@@ -25,99 +25,101 @@
   // }
   
   onMount(() => {
-    //isScrollingInterval = setInterval(isScrolling, 100);
+      //isScrollingInterval = setInterval(isScrolling, 100);
 
-    if (window.location.hash.substr(1)) {
-      parseHash();
-      if (hashParams.page) {
-	let n = Number(hashParams.page);
-	let sections = document.querySelectorAll('section');
-	if (sections[n-1]) {
-	  sections[n-1].scrollIntoView(true);
-	  //sections[n-1].focus();
-	}
+      if (window.location.hash.substr(1)) {
+          parseHash();
+          if (hashParams.page) {
+              let n = Number(hashParams.page);
+              let sections = document.querySelectorAll('section');
+              if (sections[n-1]) {
+                  sections[n-1].scrollIntoView(true);
+                  //sections[n-1].focus();
+              }
+          }
+      } else {
+          window.location.hash = "page=2";
+          let n = 2;
+          let sections = document.querySelectorAll('section');
+          if (sections[n-1]) {
+              sections[n-1].scrollIntoView(true);
+              //sections[n-1].focus();
+          }
       }
-    } else {
-      window.location.hash = "page=2";
-      let n = 2;
-      let sections = document.querySelectorAll('section');
-      if (sections[n-1]) {
-	sections[n-1].scrollIntoView(true);
-	//sections[n-1].focus();
-      }
-    }
 
-    document.onkeydown = handle_key_press;
-    console.log(document.onkeypress);
+      document.onkeydown = handle_key_press;
+      console.log(document.onkeypress);
   });
 
   onDestroy(() => { 
-    if (isScrollingInterval) {
-      clearInterval(isScrollingInterval);
-      isScrollingInterval = false;
-    }
-    if (browser) {
-      document.onkeydown = undefined;
-    }
+      if (isScrollingInterval) {
+          clearInterval(isScrollingInterval);
+          isScrollingInterval = false;
+      }
+      if (browser) {
+          document.onkeydown = undefined;
+      }
   });
 
   let hashParams = {};
   function parseHash() {
-    let hash = window.location.hash.substr(1);
-    hashParams = hash.split('&').reduce(function (result, item) {
-      let parts = item.split('=');
-      if (typeof parts[1] === 'undefined') {
-	parts[1] = true;
-      }
-      result[parts[0]] = parts[1];
-      return result;
-    }, {});
+      let hash = window.location.hash.substr(1);
+      hashParams = hash.split('&').reduce(function (result, item) {
+          let parts = item.split('=');
+          if (typeof parts[1] === 'undefined') {
+              parts[1] = true;
+          }
+          result[parts[0]] = parts[1];
+          return result;
+      }, {});
   }
 
   function handle_key_press(event) {
-    let keycode = typeof event !== 'undefined' ? event.keyCode : event.which;
-    //console.log(keycode)
-    if (keycode === 37) {
-      scroll_right();
-    }
-    if (keycode === 39) {
-      scroll_left();
-    }
+      let keycode = typeof event !== 'undefined' ? event.keyCode : event.which;
+      if (event.target && event.target.type === "text") {
+          return;
+      }
+      if (keycode === 37) {
+          scroll_right();
+      }
+      if (keycode === 39) {
+          scroll_left();
+      }
   }
 
   function scroll_right() {
-    console.log('right');
-    if (!sectionholder) {
-      sectionholder = document.querySelector('#sectionholder');
-    }
-    //sectionholder.scrollLeft -= window.innerWidth;
-    sectionholder.scrollBy({ left: -window.innerWidth, behavior: 'smooth' });
-    // note: when you wanna make it scroll smoothly in safari too see
-    // https://stackoverflow.com/questions/56011205/is-there-a-safari-equivalent-for-scroll-behavior-smooth
+      console.log('right');
+      if (!sectionholder) {
+          sectionholder = document.querySelector('#sectionholder');
+      }
+      //sectionholder.scrollLeft -= window.innerWidth;
+      sectionholder.scrollBy({ left: -window.innerWidth, behavior: 'smooth' });
+      // note: when you wanna make it scroll smoothly in safari too see
+      // https://stackoverflow.com/questions/56011205/is-there-a-safari-equivalent-for-scroll-behavior-smooth
   }
 
   function scroll_left() {
-    console.log('left');
-    if (!sectionholder) {
-      sectionholder = document.querySelector('#sectionholder');
-    }
-    //sectionholder.scrollLeft += window.innerWidth;
-    sectionholder.scrollBy({ left: window.innerWidth, behavior: 'smooth' });
+      console.log('left');
+      if (!sectionholder) {
+          sectionholder = document.querySelector('#sectionholder');
+      }
+      //sectionholder.scrollLeft += window.innerWidth;
+      sectionholder.scrollBy({ left: window.innerWidth, behavior: 'smooth' });
   }
 
   let sectionholder;
   let scrollLeft;
   function isScrolling() {
-    if (!sectionholder) {
-      sectionholder = document.querySelector('#sectionholder');
-      scrollLeft = sectionholder.scrollLeft;
-    }
+      if (!sectionholder) {
+          sectionholder = document.querySelector('#sectionholder');
+          scrollLeft = sectionholder.scrollLeft;
+      }
 
-    if (sectionholder.scrollLeft != scrollLeft) {
-      //console.log('moved! (index.svelte)');
-      scrollLeft = sectionholder.scrollLeft;
-      //sectionholder.scrollTop = 0;
-    }
+      if (sectionholder.scrollLeft != scrollLeft) {
+          //console.log('moved! (index.svelte)');
+          scrollLeft = sectionholder.scrollLeft;
+          //sectionholder.scrollTop = 0;
+      }
   }
 </script>
 
@@ -169,7 +171,7 @@
 <style>
   #sectionholder {
     font-family: sans-serif;
-    scroll-snap-type: x mandatory;	
+    scroll-snap-type: x mandatory;      
     overscroll-behaviour: contain;
     /*scroll-behavior: smooth;*/
     display: flex;
