6cd9b3f7b9b56a6e21967757746d51db527223b1
Jon Ferguson <jon@jef.us>
2023-05-04 11:24:31 -0400

    v0.6.2-beta

--- client/build/_app/immutable/chunks/1.33c9a548.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.bb4761dd.js";export{t as component};
--- client/build/_app/immutable/chunks/2.d39aadb3.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.b08335f6.js";export{t as component};
