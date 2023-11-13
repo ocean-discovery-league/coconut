6a0647a1a19b841410390371938463954b46a155
Jon Ferguson <jon@jef.us>
2023-11-13 16:47:18 -0500

    v0.8.4

--- client/build/_app/immutable/chunks/1.4275a0d2.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.63414b9c.js";export{t as component};
---
+++ client/build/_app/immutable/chunks/1.dedc9e62.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.52c1317d.js";export{t as component};
